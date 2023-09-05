7e0944c41e268fb493bd73475db724efe18f0ce9
Marcelo Severo <git@github.com:marceloses/APICatalogo.git>
2023-09-04 22:19:26 -0300

    Versao_2

--- GitJava/src/main/java/org/example/Main.java
+++ GitJava/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world, Vai corinthians!");
+        System.out.println("Hello world, vai corinthians é nois!");
     }
 }
